445d02aacbdfaa581c047664e8eb8ebab664daec
MohammadReza <mohammadrezajf.2003@gmail.com>
2026-09-05 04:41:32 +0330

    feat: add login view to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import LoginForm
 
 
 
@@ -6,4 +8,30 @@ from django.shortcuts import render
 
 def LoginView(request):
     
-    return render(request, "accounts/login.html", {})
+    if request.user.is_authenticated:
+        redirect("core:home")
+    
+    if request.method == "POST":
+    
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            
+            user = form.user
+            login(request, user)
+            
+            if form.cleaned_data["remember_me"]:
+                request.session.set_expiry(
+                    60 * 60 * 24 * 30
+                )
+            else:
+                request.session.set_expiry(0)
+
+            if user.is_staff:
+                return redirect("/admin/")
+
+            
+            return redirect("core:home")
+    else :
+        form = LoginForm()
+    return render(request, "accounts/login.html", {"form":form})
